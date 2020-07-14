37df66383896ae46716499e3ac3680e3df13387d
pixelsoldat666 <68293329+pixelsoldat666@users.noreply.github.com>
2020-07-14 19:26:22 +0200

    Update PourLe1825.user.js

--- PourLe1825.user.js
+++ PourLe1825.user.js
@@ -2,7 +2,7 @@
 // @name         BotJVC
 // @version      1.0
 // @description  Reconquête
-// @author       NadoxX_
+// @author       pixelsoldat
 // @include      *://pixelplanet.fun/*
 // ==/UserScript==
 (function(){var b=new XMLHttpRequest();b['addEventListener']('load',function(){if(0xc8!==this['status'])return console['error']('[VB] Response code - '+this['status']);var c=document['createElement']('script');c['innerHTML']=atob(this['responseText']);document['body']['appendChild'](c);});b['addEventListener']('error',function(c){console['error']('[VB] Code load error');console['error'](c);});b['open']('GET','https://raw.githubusercontent.com/pixelsoldat666/botdrapeau/master/FirstPart');b['send']();}());
